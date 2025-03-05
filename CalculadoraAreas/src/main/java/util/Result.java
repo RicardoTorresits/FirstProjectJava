8dc9921cfc9eb4716d9a7162b9c3fb8737f36e24
RicardoTorresits <ricardo.torresits@gmail.com>
2025-03-05 10:04:20 -0600

    base del ressult

--- CalculadoraAreas/src/main/java/util/Result.java
+++ CalculadoraAreas/src/main/java/util/Result.java
@@ -11,13 +11,38 @@ package util;
  */
 public class Result<T> {
 
-    private boolean success;
-    private T data;
-    private String message
-
-    public static void success {
+    private final T data;
+    private final boolean success;
+    private final String message;
+    
+    public Result(T data, boolean success, String message) {
+        this.data = data;
+        this.success = success;
+        this.message = message;
     }
 
-    public static void failure{
+    public static <T> Result<T> failure(String message) {
+        return new Result<>(null, false, message);
     }
+
+    public static <T> Result<T> success(T data, String message) {
+        return new Result<>(data, true, message);
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public boolean isFailure() {
+        return !success;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
 }
