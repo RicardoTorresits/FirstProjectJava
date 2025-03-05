e8193f4af341fea7b11db87f3fa60ae8377bd726
RicardoTorresits <ricardo.torresits@gmail.com>
2025-03-05 09:34:54 -0600

    primer commit

---
+++ CalculadoraAreas/src/main/java/com/mycompany/calculadoraareas/CalculadoraAreas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadoraareas;
+
+/**
+ *
+ * @author it-seekers
+ */
+public class CalculadoraAreas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
